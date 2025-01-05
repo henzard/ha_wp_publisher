730e2efa46ba5a2af9d746b35af2349e28c81c8e
Henzard Kruger <henzardkruger@gmail.com>
2025-01-05 15:29:31 +0200

    Updated Code

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -7,6 +7,11 @@ from unittest.mock import patch
 
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_registry import EntityRegistry
+from homeassistant.helpers.device_registry import DeviceRegistry
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.const import CONF_NAME
+from homeassistant.setup import async_setup_component
 
 from ha_wp_publisher.config_flow import WPConfigFlow
 from ha_wp_publisher.const import (
@@ -19,8 +24,6 @@ from ha_wp_publisher.const import (
     CONF_PUBLISH_INTERVAL,
     CONF_ENTITIES
 )
-
-from tests.common import MockConfigEntry
 
 
 @pytest.fixture
@@ -47,10 +50,8 @@ async def test_full_user_flow(hass: HomeAssistant):
     }
 
     with patch(
-        "custom_components.ha_wp_publisher.config_flow.WPConfigFlow.async_step_advanced",
-        return_value=await hass.config_entries.flow.async_init(
-            DOMAIN, context={'source': config_entries.SOURCE_USER}, data=user_input_step1
-        )
+        "ha_wp_publisher.config_flow.WPConfigFlow.async_step_advanced",
+        return_value={"type": "form", "step_id": "advanced"}
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"], user_input_step1
@@ -67,10 +68,8 @@ async def test_full_user_flow(hass: HomeAssistant):
     }
 
     with patch(
-        "custom_components.ha_wp_publisher.config_flow.WPConfigFlow.async_step_sensors",
-        return_value=await hass.config_entries.flow.async_init(
-            DOMAIN, context={'source': config_entries.SOURCE_USER}, data=user_input_step2
-        )
+        "ha_wp_publisher.config_flow.WPConfigFlow.async_step_sensors",
+        return_value={"type": "form", "step_id": "sensors"}
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"], user_input_step2
@@ -85,19 +84,11 @@ async def test_full_user_flow(hass: HomeAssistant):
     }
 
     with patch(
-        "custom_components.ha_wp_publisher.config_flow.WPConfigFlow.async_create_entry",
+        "ha_wp_publisher.config_flow.WPConfigFlow.async_create_entry",
         return_value=config_entries.ConfigFlowResult(
             type=config_entries.ConfigFlowResultType.CREATE_ENTRY,
             title="HA to WP Publisher",
-            data={
-                CONF_WP_URL: "https://example.com",
-                CONF_WP_USER: "username",
-                CONF_WP_PASSWORD: "password",
-                CONF_POST_TYPE: "sensor_data",
-                CONF_CUSTOM_FIELDS: json.dumps({"field1": "value1", "field2": "value2"}),
-                CONF_PUBLISH_INTERVAL: 60,
-                CONF_ENTITIES: ["sensor.temperature", "sensor.humidity"]
-            }
+            data={**user_input_step1, **user_input_step2, **user_input_step3}
         )
     ):
         result = await hass.config_entries.flow.async_configure(
