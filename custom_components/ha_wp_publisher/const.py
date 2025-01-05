fd702a44c2e9481b7c23ddf6784137df239432f8
Henzard Kruger <henzardkruger@gmail.com>
2025-01-05 14:40:07 +0200

    Update const.py

--- custom_components/ha_wp_publisher/const.py
+++ custom_components/ha_wp_publisher/const.py
@@ -1,6 +1,17 @@
+"""
+Constants for the ha_wp_publisher integration.
+"""
+
 DOMAIN = "ha_wp_publisher"
-PLATFORMS = []
+
+# Config Flow / Entry Data Keys
 CONF_WP_URL = "wp_url"
 CONF_WP_USER = "wp_user"
 CONF_WP_PASSWORD = "wp_password"
+CONF_POST_TYPE = "post_type"
+CONF_CUSTOM_FIELDS = "custom_fields"
+CONF_PUBLISH_INTERVAL = "publish_interval"
 CONF_ENTITIES = "entities"
+
+# List of platforms this integration provides:
+PLATFORMS = ["sensor"]
